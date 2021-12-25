48780aa54640b2c53cc5b5f66f2e70ae8260bdc6
ahmethalac <ahmetfeyzihalac@gmail.com>
2021-12-26 00:17:19 +0300

    more configurations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import React from 'react';
 
-import { Provider } from 'react-redux'
-import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux';
+import type { AppProps } from 'next/app';
 
-import store from '../app/store'
+import store from '../app/store';
 import { UserProvider } from '@auth0/nextjs-auth0';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <UserProvider>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    </UserProvider>
-  )
+	return (
+		<UserProvider>
+			<Provider store={store}>
+				<Component {...pageProps} />
+			</Provider>
+		</UserProvider>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import React from 'react';
 
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
 const IndexPage: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Redux Toolkit</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <a href="/api/auth/login">Login</a>
-    </div>
-  )
-}
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Redux Toolkit</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<a href="/api/auth/login">Login</a>
+		</div>
+	);
+};
 
-export default IndexPage
+export default IndexPage;
