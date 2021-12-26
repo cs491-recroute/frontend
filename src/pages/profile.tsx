40251cced72f2882592e2dbfa135116e210e0e62
ahmethalac <ahmetfeyzihalac@gmail.com>
2021-12-26 22:10:35 +0300

    initial version of profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,20 +1,43 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import Head from 'next/head';
 import { useUser } from '@auth0/nextjs-auth0';
 import { NextPage } from 'next';
+import styles from '../styles/Profile.module.css';
+import { EuiButton } from '@elastic/eui';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
 
 const ProfilePage: NextPage = () => {
 	const { user } = useUser();
+	const router = useRouter();
+	const handleLogout = () => {
+		router.push('/api/auth/logout');
+	};
 
 	if (user) {
 		return (
-			<div>
+			<div className={styles.container}>
 				<Head>
 					<title>My Profile</title>
 					<link rel="icon" href="/favicon.ico" />
 				</Head>
-      Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+				<div className={styles.header}>
+					<div className={styles.user}>
+						<img src={user.picture || ''} className={styles.avatar}/>
+						{user.name}
+					</div>
+					<div className={styles.title}>My Profile</div>
+					<EuiButton
+						onClick={handleLogout}
+						fill
+						color='primary'
+					>
+						Logout
+					</EuiButton>
+				</div>
 			</div>
 		);
 	}
