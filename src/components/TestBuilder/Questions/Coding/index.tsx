bdff07a4e28a27a33853e2255f2e8616bcfff77f
ahmethalac <ahmetfeyzihalac@gmail.com>
2022-05-03 19:30:07 +0300

    Test Content :: Coding :: make description formatted

--- src/components/TestBuilder/Questions/Coding/index.tsx
+++ src/components/TestBuilder/Questions/Coding/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React, { useState, forwardRef, RefObject } from 'react';
 import CodeEditor from '../../../CodeEditor';
-import { EuiFormRow } from '@elastic/eui';
+import { EuiFormRow, EuiText } from '@elastic/eui';
 import classNames from 'classnames';
 import styles from './Coding.module.scss';
 import { RefProps } from '../../../CodeEditor';
@@ -25,18 +25,25 @@ const Coding = forwardRef(({ description, editMode, number, _id, testID }: Rende
         setResult(runResult);
     };
 
-    return <EuiFormRow label={`${number}. ${description}`} fullWidth className={classNames({ [styles.fullScreen]: fullScreen, [styles.darkMode]: darkMode })}>
-        <CodeEditor 
-            ref={ref as RefObject<RefProps>}
-            editMode={editMode}
-            fullScreen={fullScreen}
-            onRunCode={onRunCode}
-            onFullScreen={() => setFullScreen(!fullScreen)}
-            onDarkModeChange={setDarkMode}
-            result={result}
-            uniqueID={_id}
-        />
-    </EuiFormRow>
+    return <>
+        <EuiText size='xs'>
+            <pre>
+                {`${number}. ${description}`}
+            </pre>
+        </EuiText>
+        <EuiFormRow fullWidth className={classNames({ [styles.fullScreen]: fullScreen, [styles.darkMode]: darkMode })}>
+            <CodeEditor 
+                ref={ref as RefObject<RefProps>}
+                editMode={editMode}
+                fullScreen={fullScreen}
+                onRunCode={onRunCode}
+                onFullScreen={() => setFullScreen(!fullScreen)}
+                onDarkModeChange={setDarkMode}
+                result={result}
+                uniqueID={_id}
+            />
+        </EuiFormRow>
+    </>
 });
 
 Coding.displayName = 'Coding';
